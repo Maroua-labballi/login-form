ba8b573a5fa70976007539852235514e8e9c70b7
Maroua-labballi <maroualab5@gmail.com>
2022-08-23 16:51:45 +0100

    login

--- login form 2/java.js
+++ login form 2/java.js
@@ -3,9 +3,15 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const contactChoice = document.getElementById('contactChoice');
+const Gender = document.getElementById('Gender');
+
+
+
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
+
 	checkInputs();
 });
 
@@ -14,8 +20,10 @@ function checkInputs() {
 	const usernameValue = username.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
-	const password2Value = password2.value.trim();
-	
+	const password2Value = password2.value.trim();  
+	var getSelectedValue = document.querySelector( 'input[name="contact"]:checked');  
+	var getSelectedValue = document.querySelector( 'input[name="gender"]:checked');  
+
 	if(usernameValue === '') {
 		setErrorFor(username, 'Username cannot be blank');
     } else if(usernameValue.length < 3 ) {
@@ -48,19 +56,43 @@ function checkInputs() {
 		setSuccessFor(password2);
 	}
 
-    if(GenderValue === '') {
-		setErrorFor(Gender, 'Gender cannot be blank');
-    } else if(GenderValue != "female" || GenderValue != "male" ) {
-		setErrorFor(Gender, 'Gender is not correct');
-	} else{
-		setSuccessFor(Gender);
+
+	if(getSelectedValue != null) {   
+		document.getElementById("contactChoice").innerHTML=getSelectedValue.value +(" is selected");  
+	} else{  
+		document.getElementById("error").innerHTML= "*You have not selected any choice";    
 	}
 
-    if(PhoneNumberValue === '') {
-		setErrorFor(PhoneNumber, 'PhoneNumber cannot be blank');
-	} else{
-		setSuccessFor(PhoneNumber);
+
+	if(getSelectedValue != null) {   
+		document.getElementById("Gender").innerHTML=getSelectedValue.value +(" is selected");  
+	} else{  
+		document.getElementById("error").innerHTML= "*You have not selected any choice";    
 	}
+
+	
+
+	// document.getElementById('contactChoice').onclick = function() {
+	// var markedCheckbox = document.getElementsByName('contact');
+	// for (var checkbox of markedCheckbox) {
+	// if (checkbox.checked)
+	// document.body.append(checkbox.value + ' ');
+	// }
+	// }
+
+    // if(GenderValue === '') {
+	// 	setErrorFor(Gender, 'Gender cannot be blank');
+    // } else if(GenderValue != "female" || GenderValue != "male" ) {
+	// 	setErrorFor(Gender, 'Gender is not correct');
+	// } else{
+	// 	setSuccessFor(Gender);
+	// }
+
+    // if(PhoneNumberValue === '') {
+	// 	setErrorFor(PhoneNumber, 'PhoneNumber cannot be blank');
+	// } else{
+	// 	setSuccessFor(PhoneNumber);
+	// }
 }
 
 function setErrorFor(input, message) {
